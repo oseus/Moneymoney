8572c7909ac6a2525a5b9b0cf9ac1beeb2df249c
misterd <coat.david@gmail.com>
2018-05-07 15:04:44 +0200

    test

---
+++ even/spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    description "MyText"
+    date "2018-05-07 10:17:25"
+    place "MyString"
+  end
+end
---
+++ even/spec/factories/students_teachers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :students_teacher do
+    user nil
+    event nil
+  end
+end
